86bcd83639f7393952f4896e58728e6567725efd
KJ1127 <tav575450@gmail.com>
2026-04-08 00:49:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def on_message(message):
             "⚔️ **ĐỘC CHIÊU!**",
             file=discord.File("gif/docchieu.gif")
         )
-    elif msg == ["căng thế", "chill đi", "chill","ayo chill"]:
+    elif msg in ["căng thế", "chill đi", "chill","ayo chill"]:
         await message.channel.send(
             "bỉnh tõm đuy bờ rô🙂‍↕️",
             file=discord.File("gif/suwa_lowcortisol.gif")
